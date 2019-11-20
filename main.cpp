53f1cdd04af5f9245d2d0a5f027d8b691dffe96e
Felix <none@none>
2019-11-20 17:16:45 +0100

    first implementation of CMakeLists.txt file for both operating systems
    --HG--
    branch : IDE

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
-#include "Application.h"
+//#include "Application.h"
 
 int main() {
 
-    Application* app = new Application();
+    /*Application* app = new Application();
     app->initialize();
 
     while (app->run())
     {
-    }
+    }*/
+    std::cout << "Hallo World" << std::endl;
     return 0;
 }

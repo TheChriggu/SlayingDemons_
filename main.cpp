575c00c8e553652d270b9c2b5f3708db6893677e
Felix <none@none>
2019-11-21 01:42:46 +0100

    Modified CMakeLists.txt so that the Project builds on all platforms
    - Removed old dll Folder and its content
    - Removed unused NetBeans Project
    
    --HG--
    branch : IDE

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
-//#include "Application.h"
+#include "Application.h"
 
 int main() {
 
-    /*Application* app = new Application();
+    Application* app = new Application();
     app->initialize();
 
     while (app->run())
     {
-    }*/
-    std::cout << "Hallo World" << std::endl;
+    }
     return 0;
 }

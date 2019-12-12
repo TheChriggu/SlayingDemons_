b8cd936d3b68c6942492c01d52c41be69a1ba557
christian.heusser <none@none>
2019-12-12 10:26:45 +0100

    Fixed references to wrong folder
    --HG--
    branch : DungeonNavigation

---
+++ Src/Dungeon/Floor.h
@@ -0,0 +1,15 @@
+//
+// Created by christian.heusser on 12.12.2019.
+//
+
+#ifndef SLAYINGDEMONS_FLOOR_H
+#define SLAYINGDEMONS_FLOOR_H
+
+
+namespace sd {
+    class Floor {
+
+    };
+}
+
+#endif //SLAYINGDEMONS_FLOOR_H

6977a5489d5f9eab683b91d76c82f2c46ee3de92
Lara <none@none>
2020-02-12 13:22:32 +0100

    added dungeon 3
    --HG--
    branch : Dungeon

---
+++ Resources/Scripts/dungeon_3.lua
@@ -0,0 +1,16 @@
+is_in_green_room = false
+
+
+function input_received (input)
+    if input=="Inspect Room" and is_in_green_room then
+        print_line("[color=255,0,144,255]Test")
+    end
+end
+
+function room_changed(name)
+    if name=="green_room" then
+        is_in_green_room = true
+    else
+        is_in_green_room = false
+    end
+end

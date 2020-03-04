f43baf6df0d5f0ea225973f0f8d868170763a2ef
christian.heusser <none@none>
2020-03-04 12:18:55 +0100

    Made it so the audio player can play sfx, and change to any .ogg file in audio folder
    --HG--
    branch : Audio

--- Src/Audio/AudioPlayer.cpp
+++ Src/Audio/AudioPlayer.cpp
@@ -4,13 +4,42 @@
 
 #include <iostream>
 #include "AudioPlayer.h"
+#include "../ScriptEngine/ScriptEngine.h"
 
-AudioPlayer::AudioPlayer() {
+sd::AudioPlayer::AudioPlayer() {
+    ScriptEngine::get().register_all ("set_background_music", &AudioPlayer::set_background_music, this);
+    ScriptEngine::get().register_all ("trigger_sfx", &AudioPlayer::trigger_sfx, this);
+
     background_music_ = std::make_shared<sf::Music>();
+    sound_ = std::make_shared<sf::Sound>();
+    sound_buffer_ = std::make_shared<sf::SoundBuffer>();
+
+
+
     if (!background_music_->openFromFile("../Resources/Audio/fantasy.ogg"))
     {
         std::cout << "could not open background music\n";
     }
     background_music_->setLoop(true);
     background_music_->play();
+    current_music_ = "fantasy";
+}
+
+void sd::AudioPlayer::set_background_music(std::string music) {
+    if(music != current_music_)
+    {
+        current_music_ = music;
+        if (!background_music_->openFromFile("../Resources/Audio/" + music + ".ogg"))
+        {
+            std::cout << "could not open background music\n";
+        }
+        background_music_->setLoop(true);
+        background_music_->play();
+    }
+}
+
+void sd::AudioPlayer::trigger_sfx(std::string sfx) {
+    sound_buffer_->loadFromFile("../Resources/Audio/" + sfx + ".ogg");
+    sound_->setBuffer(*sound_buffer_);
+    sound_->play();
 }
--- Src/Audio/AudioPlayer.h
+++ Src/Audio/AudioPlayer.h
@@ -8,14 +8,22 @@
 #include "SFML/Audio.hpp"
 #include "../Core/GlobalDefinitions.h"
 
-class AudioPlayer {
-private:
-    Sp<sf::Music> background_music_;
+namespace sd {
+    class AudioPlayer {
+    private:
+        Sp<sf::Music> background_music_;
+        std::string current_music_;
+        Sp<sf::SoundBuffer> sound_buffer_;
+        Sp<sf::Sound> sound_;
 
-public:
-    AudioPlayer();
-    ~AudioPlayer() = default;
-};
+    public:
+        AudioPlayer();
 
+        ~AudioPlayer() = default;
+
+        void set_background_music(std::string music);
+        void trigger_sfx(std::string sfx);
+    };
+}
 
 #endif //SLAYINGDEMONS_AUDIOPLAYER_H
